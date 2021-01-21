9e1619a4f5d83fa7d8c8676c55e1618ef6cc807e
BGazsi <bgazsi94@gmail.com>
2021-01-21 19:47:58 +0100

    init db

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
-const sensor = require("node-dht-sensor").promises;
+require('dotenv').config()
+const useRealSensor = process.env.use_real_sensor
+const Cloudant = require('@cloudant/cloudant');
+
+const cloudant = new Cloudant({
+  url: process.env.cloudant_url,
+  plugins: [
+    {
+      iamauth: {
+        iamApiKey: process.env.iam_api_key
+      }
+    }
+  ]
+})
+
+const dbClient = cloudant.use('temperatures');
+
+let sensor = {
+  read: () => new Promise((resolve, reject) => {
+    resolve({ temperature: 22.600000381469727, humidity: 48 })
+  })
+}
+
+if (useRealSensor && useRealSensor !== 'false') {
+  sensor = require("node-dht-sensor").promises;
+}
+
 
 async function exec() {
   try {
-    console.log('res elott')
     const res = await sensor.read(22, 4);
-    console.log('res van')
+    const document = {
+      temp: res.temperature.toFixed(1),
+      humidity: res.humidity.toFixed(1),
+    }
     console.log(
       `temp: ${res.temperature.toFixed(1)}°C, ` +
-        `humidity: ${res.humidity.toFixed(1)}%`
+      `humidity: ${res.humidity.toFixed(1)}%`
     );
+    dbClient.insert(document, new Date().getTime()).then(data => {
+      console.log('data', data);
+    }).catch(err => {
+      console.log('db error', err)
+    })
   } catch (err) {
-    console.log('catch');
     console.error("Failed to read sensor data:", err);
   }
 }
